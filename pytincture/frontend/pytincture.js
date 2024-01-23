88f41101713ab33368e2fa5f96fb95932fadfb11
Stephen Chapman <schapman1974@gmail.com>
2024-01-23 13:40:34 -0500

    data changes

--- pytincture/frontend/pytincture.js
+++ pytincture/frontend/pytincture.js
@@ -9,6 +9,5 @@ async function runTinctureApp(application, widgetlib){
     let appResponse = await fetch("appcode/appcode.pyt");
     let appBinary = await appResponse.arrayBuffer();
     pyodide.unpackArchive(appBinary, "zip");
-    let pyApp = (await fetch("appcode/"+application+".py")).text();
-    pyodide.runPython((await pyApp).toString()+"\n"+application+"()");
+    pyodide.runPython("from " + application + " import " + application + " as app\napp()");
 }
