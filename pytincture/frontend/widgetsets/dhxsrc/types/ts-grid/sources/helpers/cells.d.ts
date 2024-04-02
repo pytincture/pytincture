bd86ece5674cee441424d16c52058f8c2ecf3c06
Stephen Chapman <schapman1974@gmail.com>
2024-04-01 23:03:36 -0400

    rem

--- pytincture/frontend/widgetsets/dhxsrc/types/ts-grid/sources/helpers/cells.d.ts
+++
@@ -1,5 +0,0 @@
-import { ICol, IRow } from "./../types";
-import { Id } from "../../../ts-common/types";
-export declare function getWidth(columns: ICol[], colspan: number, index: number): number;
-export declare function getHeight(dataRows: IRow[], rowspan: number, index: number): number;
-export declare function getReducedColspan(columns: ICol[], colId: Id, colspan: number): number;
